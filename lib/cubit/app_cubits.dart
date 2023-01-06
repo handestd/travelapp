dcef6c8f46e0bc9f6e803df809fcecadf99bac67
MEO MEO <handestd@gmail.com>
2023-01-06 12:51:16 +0700

    Initial commit

---
+++ lib/cubit/app_cubits.dart
@@ -0,0 +1,33 @@
+
+import 'package:bloc/bloc.dart';
+import 'package:travelapp/cubit/app_cubit_states.dart';
+import 'package:travelapp/model/data_model.dart';
+import 'package:travelapp/services/data_services.dart';
+
+class AppCubits extends Cubit<CubitStates> {
+  AppCubits({required this.data}) : super (InitialState()) {
+    emit(WelcomeState());
+  }
+  final DataServices data;
+  late final places;
+  void getData() async {
+    try {
+      emit(LoadingState());
+      places = await data.getInfo();
+      emit(LoadedState(places));
+    }
+    catch(e)
+    {
+
+    }
+  }
+
+  detailPage(DataModel data) {
+    emit(DetailState(data));
+  }
+
+  goHome() {
+    emit(LoadedState(places));
+  }
+
+}
---
+++ lib/pages/detail_page.dart
@@ -0,0 +1,174 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:travelapp/cubit/app_cubit_states.dart';
+import 'package:travelapp/cubit/app_cubits.dart';
+import 'package:travelapp/misc/colors.dart';
+import 'package:travelapp/widgets/app_buttons.dart';
+import 'package:travelapp/widgets/app_large_text.dart';
+import 'package:travelapp/widgets/app_text.dart';
+import 'package:travelapp/widgets/responsive_button.dart';
+
+class DetailPage extends StatefulWidget {
+  const DetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  int gottenStars = 3;
+  int selectedIndex = -1;
+  @override
+  Widget build(BuildContext context) {
+    return  BlocBuilder<AppCubits, CubitStates>(
+      builder: (context, state) {
+        DetailState detail = state as DetailState;
+        return Scaffold(
+          body: Container(
+            width: double.maxFinite,
+            height: double.maxFinite,
+            child: Stack(
+              children: [
+                Positioned(
+                    left: 0,
+                    right: 0,
+                    child:
+                    Container(
+                      width: double.maxFinite,
+                      height: 270,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: NetworkImage("http://mark.bslmeiyu.com/uploads/" + detail.place.img),
+                              fit: BoxFit.cover
+                          )
+                      ),
+
+                    )),
+                Positioned(
+                    left: 20,
+                    top: 50,
+                    child:
+                    Row(
+                      children: [
+                        IconButton(
+                          onPressed: ()
+                          {
+                            BlocProvider.of<AppCubits>(context).goHome();
+                          },
+                          icon: Icon(Icons.menu),
+                          color: Colors.white,),
+
+
+                      ],
+                    )
+                ),
+                Positioned(
+                    top: 250,
+                    child:
+                    Container(
+                      padding: const EdgeInsets.only(left: 20, right: 20, top: 30),
+                      width: MediaQuery.of(context).size.width,
+                      height: 500,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(topRight: Radius.circular(30), topLeft: Radius.circular(30))
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              AppLargeText(text: detail.place.name, color: Colors.black,),
+                              AppLargeText(text: "\$ " + detail.place.price.toString(), color: AppColors.mainColor,),
+
+
+                            ],
+                          ),
+                          SizedBox(height: 10,),
+                          Row(
+                            children: [
+                              Icon(Icons.location_on, color: AppColors.mainColor,),
+                              SizedBox(width: 5,),
+                              AppText(text: detail.place.location, color: AppColors.textColor1,)
+                            ],
+                          ),
+                          SizedBox(height: 20,),
+                          Row(
+                            children: [
+                              Wrap(
+                                children: List.generate(5, (index) {
+                                  return Icon(Icons.star, color: index<detail.place.stars? AppColors.starColor:AppColors.textColor2,);
+                                }),
+                              ),
+                              SizedBox(width: 10,),
+                              AppText(text: "(${detail.place.stars}.0)", color: AppColors.textColor2,)
+                            ],
+                          ),
+                          SizedBox(height: 25,),
+                          AppLargeText(text: "People", color: Colors.black.withOpacity(0.8),size: 20,),
+                          SizedBox(height: 10,),
+                          AppText(text: "Number of people in your group", color: AppColors.mainTextColor,),
+                          SizedBox(height: 10,),
+                          Wrap(
+                            children:
+                            List.generate(5, (index) {
+                              return InkWell(
+                                onTap: () {
+                                  setState(() {
+                                    selectedIndex = index;
+                                  });
+                                },
+                                child: Container(
+                                  margin: const EdgeInsets.only(right: 10),
+                                  child: AppButtons(
+                                    size: 50,
+                                    color: selectedIndex==index ? Colors.white : Colors.black,
+                                    backgroundColor: selectedIndex==index ? Colors.black : AppColors.buttonBackground,
+                                    borderColor: selectedIndex == index ? Colors.black : AppColors.buttonBackground,
+                                    isIcon:false,
+                                    text: (index+1).toString(),
+                                  ),
+                                ),
+                              );
+                            })
+                            ,
+                          ),
+                          SizedBox(height: 20,),
+                          AppLargeText(text: "Description", color: Colors.black.withOpacity(0.8),),
+                          SizedBox(height: 10,),
+                          AppText(text: detail.place.description, color: AppColors.mainTextColor,),
+
+                        ],
+                      ),
+                    )),
+                Positioned(
+                  bottom: 20,
+                  left: 20,
+                  right: 20,
+                  child: Row(
+                    children: [
+                      AppButtons(
+                          isIcon: true,
+                          icon: Icons.favorite_border,
+                          size: 60,
+                          color: AppColors.textColor2,
+                          backgroundColor: Colors.white,
+                          borderColor: AppColors.textColor2),
+                      SizedBox(width: 20,),
+                      ResponsiveButton(isResponsive: true,),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      }
+
+    );
+  }
+}
